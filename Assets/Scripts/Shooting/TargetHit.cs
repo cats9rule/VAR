7f7c9255b8ecbe6eb8c9417d7c57f31bc30dc1e5
Nevena Tufegdzic <nevenatufegdzic9@gmail.com>
2023-08-20 16:58:28 +0200

    Layout finished

--- Assets/Scripts/Shooting/TargetHit.cs
+++ Assets/Scripts/Shooting/TargetHit.cs
@@ -1,32 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TargetHit : MonoBehaviour
 {
     private Vector3 targetCenter;
     private float targetRadius;
+    private int totalPoints;
+
+    public TMP_Text scoreDisplay;
 
     void Start()
     {
-        Debug.Log("Started script");
         targetCenter = transform.position;
         targetCenter.z = 0;
         targetRadius = transform.localScale.x / 2;
+        totalPoints = 0;
+        scoreDisplay.text = $"Score: {totalPoints}";
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        Vector3 contactPoint = other.GetContact(0).point;
-        Debug.Log($"Target hit at point ({contactPoint.x}, {contactPoint.y}, {contactPoint.z})");
-        contactPoint.z = 0;
-        Debug.Log(Vector3.Distance(contactPoint, targetCenter));
+        if (!other.gameObject.CompareTag("Bullet")) return;
 
+        Vector3 contactPoint = other.GetContact(0).point;
+        contactPoint.z = 0;
         decimal points = Math.Floor(Convert.ToDecimal(Vector3.Distance(contactPoint, targetCenter) / targetRadius * 5 - 5) * -1 + 1);
         points = points < 6 ? points : 5;
         
         Debug.Log($"Points: {points}");
 
+        totalPoints += Convert.ToInt32(points);
+
+        scoreDisplay.text = $"Score: {totalPoints}";
     }
 }
